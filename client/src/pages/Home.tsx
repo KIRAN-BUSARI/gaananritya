c954b7673336b38fc9cd9640e8af3a523fe110e2
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2025-04-29 14:50:33 +0530

    style: Import Pillars component and adjust layout in Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,6 +4,7 @@ import FounderPage from './FounderPage';
 import ImageBanner from '@/components/ImageBanner';
 import banner from '@/assets/InvitationBanner.png';
 import Achievements from './Achievements';
+import Pillars from './Pillars';
 
 export default function Home() {
   return (
@@ -11,8 +12,9 @@ export default function Home() {
       <Hero />
       <Banner />
       <FounderPage />
-      <ImageBanner image={banner} />
       <Achievements />
+      <ImageBanner image={banner} />
+      <Pillars />
     </>
   );
 }
