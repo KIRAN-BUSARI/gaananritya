4f83f5bc2f364456188e7993f07d5bb856f08332
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2025-05-05 11:28:35 +0530

    refactor: adjust text size in AddressCard component for better readability

--- client/src/components/Cards/AddressCard.tsx
+++ client/src/components/Cards/AddressCard.tsx
@@ -12,13 +12,12 @@ type AddressCardProps = {
 
 const AddressCard = ({ address }: AddressCardProps) => {
   return (
-    <div className="flex w-full flex-col text-balance p-4 text-base sm:text-lg">
+    <div className="flex w-full flex-col text-balance p-4 text-sm md:text-base">
       <h2 className="font-semibold capitalize text-secondary1">
         {address.city}
       </h2>
       <p className="mt-2 font-medium">{address.name} &reg;</p>
       <div className="mt-1">
-        {/* Handle address array with or without JSX elements */}
         {Array.isArray(address.address) ? (
           address.address.map((line, idx) => <div key={idx}>{line}</div>)
         ) : (
