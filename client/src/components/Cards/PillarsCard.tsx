aaff5775dd938687c5bd72980fa0b8a6d7ddd6aa
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2025-05-12 01:00:25 +0530

    Refactor PillarsCard component styles for improved layout and consistency

--- client/src/components/Cards/PillarsCard.tsx
+++ client/src/components/Cards/PillarsCard.tsx
@@ -14,7 +14,7 @@ const PillarsCard = ({
   return (
     <div
       style={{ backgroundColor: color }}
-      className={`mx-auto flex w-10/12 flex-col gap-4 overflow-hidden rounded-lg border border-gray-50 py-6 shadow-lg transition-transform duration-300 hover:scale-[1.02] sm:flex-row sm:gap-5 md:w-full md:gap-6 md:p-6`}
+      className={`mx-auto flex w-10/12 flex-col gap-4 overflow-hidden rounded-lg border border-gray-50 p-6 shadow-lg transition-transform duration-300 hover:scale-[1.02] sm:flex-row sm:gap-5 md:w-full md:gap-6 md:p-6`}
     >
       <div
         className={`w-12/12 flex flex-col items-center justify-center md:w-7/12`}
@@ -26,10 +26,10 @@ const PillarsCard = ({
           loading="lazy"
         />
       </div>
-      <div className={`w-full content-center space-y-2 px-6 md:space-y-1`}>
+      <div className={`w-full content-center space-y-2 md:space-y-1`}>
         <h2 className="text-left text-lg font-semibold 2xl:text-2xl">{name}</h2>
-        <p className="text-left text-base italic">{designation}</p>
-        <p className="line-clamp-5 text-balance text-left text-base">
+        <p className="text-left text-sm italic md:text-base">{designation}</p>
+        <p className="line-clamp-5 text-balance text-left text-sm md:text-base">
           {description}
         </p>
       </div>
